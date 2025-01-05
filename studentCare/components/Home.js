e7f249f53347ff78292ab700c1d9e707559ba245
UserLasa73 <lasanthakumaraofficial@gmail.com>
2025-01-06 04:04:47 +0530

    imported react-navigation packages and Implemented bottomTabnavigator buttons

--- studentCare/components/Home.js
+++ studentCare/components/Home.js
@@ -1,18 +1,26 @@
 import * as React from 'react'
-import { PaperProvider, Appbar } from 'react-native-paper'
-import { StyleSheet, View, Text, ScrollView, Image } from 'react-native';
+import { PaperProvider, Appbar, Text, BottomNavigation } from 'react-native-paper'
+import { StyleSheet, View, ScrollView, Image } from 'react-native';
 import { SafeAreaView } from 'react-native';
 
+import { CommonActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function Home() {
     const uovLogoImage = require('../assets/uovLogo.jpeg');
     const goBack = () => console.log("Went back")
+
     return (
         <>
             <PaperProvider>
-                <ScrollView>
-                    <SafeAreaView>
 
+                <SafeAreaView>
+                    <ScrollView>
                         <View style={styles.container}>
 
                             <View style={styles.headerContainer}>
@@ -25,22 +33,110 @@ export default function Home() {
                                 </View>
                             </View>
 
-                            <View>
-                                
-                            </View>
 
                             <View style={styles.footer}>
                                 <Text style={styles.footerText}>Uov@2024</Text>
                             </View>
 
+
+
+                            <NavigationContainer>
+                                <View>
+                                    <Tab.Navigator
+                                        screenOptions={{
+                                            headerShown: false,
+                                        }}
+                                        tabBar={({ navigation, state, descriptors, insets }) => (
+                                            <BottomNavigation.Bar
+                                                navigationState={state}
+                                                safeAreaInsets={insets}
+                                                onTabPress={({ route, preventDefault }) => {
+                                                    const event = navigation.emit({
+                                                        type: 'tabPress',
+                                                        target: route.key,
+                                                        canPreventDefault: true,
+                                                    });
+
+                                                    if (event.defaultPrevented) {
+                                                        preventDefault();
+                                                    } else {
+                                                        navigation.dispatch({
+                                                            ...CommonActions.navigate(route.name, route.params),
+                                                            target: state.key,
+                                                        });
+                                                    }
+                                                }}
+                                                renderIcon={({ route, focused, color }) => {
+                                                    const { options } = descriptors[route.key];
+                                                    if (options.tabBarIcon) {
+                                                        return options.tabBarIcon({ focused, color, size: 24 });
+                                                    }
+
+                                                    return null;
+                                                }}
+                                                getLabelText={({ route }) => {
+                                                    const { options } = descriptors[route.key];
+                                                    const label =
+                                                        options.tabBarLabel !== undefined
+                                                            ? options.tabBarLabel
+                                                            : options.title !== undefined
+                                                                ? options.title
+                                                                : route.title;
+
+                                                    return label;
+                                                }}
+                                            />
+                                        )}
+                                    >
+                                        <Tab.Screen
+                                            name="Home"
+                                            component={HomeScreen}
+                                            options={{
+                                                tabBarLabel: 'Home',
+                                                tabBarIcon: ({ color, size }) => {
+                                                    return <Icon name="home" size={size} color={color} />;
+                                                },
+                                            }}
+                                        />
+                                        <Tab.Screen
+                                            name="Settings"
+                                            component={SettingsScreen}
+                                            options={{
+                                                tabBarLabel: 'Settings',
+                                                tabBarIcon: ({ color, size }) => {
+                                                    return <Icon name="cog" size={size} color={color} />;
+                                                },
+                                            }}
+                                        />
+                                    </Tab.Navigator>
+                                </View>
+                            </NavigationContainer>
+
                         </View>
-                    </SafeAreaView>
-                </ScrollView>
+                    </ScrollView>
+                </SafeAreaView>
+
 
             </PaperProvider>
 
         </>
     )
+}
+
+function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text variant="headlineMedium">Home!</Text>
+        </View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={styles.container}>
+            <Text variant="headlineMedium">Settings!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
