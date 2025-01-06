12bf109d9075052198c9b282b957ac8c25983785
UserLasa73 <lasanthakumaraofficial@gmail.com>
2025-01-06 23:59:11 +0530

    Updated App.js including Stack Navigator for changing from Login Screen to Home screen

--- studentCare/App.js
+++ studentCare/App.js
@@ -1,22 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home';
-import Login from './components/Login';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      {/* <Home /> */}
-      <Login />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="login"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: "#4b0150",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerTitleAlign: "center",
+            }}
+          >
+            <Stack.Screen
+              name="login"
+              component={Login}
+              options={{ title: "UoV Student Care" }}
+            />
+            <Stack.Screen
+              name="home"
+              component={Home}
+              options={{ title: "UoV Student Care" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
